3714fbc6068e625e013ef12cd6d88eedbc1a3f45
Ivana Feitosa <ivsfeitosa@gmail.com>
2024-09-12 22:31:40 -0300

    Teste de criacao de usuario

--- tests/CadastroDeUsuario.test.js
+++ tests/CadastroDeUsuario.test.js
@@ -1,3 +1,7 @@
+process.env.NODE_ENV = 'development'
+require('dotenv').config({path:'.env.test'})
+
+
 const bcrypt = require("bcryptjs");
 const { criarDados } = require("../controllers/usuarioController");
 const { usuario } = require("../models");
@@ -26,5 +30,34 @@ describe("Teste de criação de usuário", () => {
             },
         };
 
+        // Criptografia de senhas
+        const senhaCriptografada = await bcrypt.hash(req.body.senha, 8);
+
+        // Simulação de criação no banco de dados
+        const usuarioCriado = {
+            id: 1,
+            nome: req.body.nome,
+            email: req.body.email,
+            role: req.body.role,
+            senha: senhaCriptografada
+        };
+        
+        usuario.create = () => Promise.resolve(usuarioCriado);
+
+
+        // Execução da função ou método da controller
+        await criarDados(req, res);
+
+        // Verificar se a senha foi criptografada e o usuario criado
+        if(res.statusCode === 201 && res.data.message === 'Usuário cadastrado com sucesso') {
+            const senhaCorreta = await bcrypt.compare(req.body.senha, senhaCriptografada);
+            if (senhaCorreta) {
+                console.log('Teste de criação: Passou');
+            } else {
+                console.log('Teste de criação: Falhou - Senha criptografada não confere');
+            }
+        } else {
+            console.log('Teste de criação: Falhou - Resposta incorreta')
+        }
     });
 })
